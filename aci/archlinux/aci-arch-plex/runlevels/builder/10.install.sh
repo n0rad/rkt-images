a7e1ea828583f5cf84847f7aa35401510649a6e6
Arnaud Lemaire <alemaire@norad.fr>
2016-05-17 23:40:25 +0200

    remove old plex

--- aci/archlinux/aci-arch-plex/runlevels/builder/10.install.sh
+++
@@ -1,9 +0,0 @@
-#!/dgr/bin/busybox sh
-set -e
-. /dgr/bin/functions.sh
-isLevelEnabled "debug" && set -x
-
-pacman --root=/  -Syu libelf paxtest --noconfirm
-export PATH=$PATH:${ROOTFS}/usr/bin
-
-su -c 'yaourt -yyS plex-media-server --noconfirm' yaourt
