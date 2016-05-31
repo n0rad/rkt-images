d37650e39d8097acc0d6864247910920a12ef0b9
n.bouteillier <n.bouteillier@blablacar.com>
2016-05-31 22:08:26 +0200

    Fix yaourt,pacman - auto

--- aci/archlinux/aci-arch-yaourt-auto/runlevels/inherit-build-early/install.sh
+++ aci/archlinux/aci-arch-yaourt-auto/runlevels/inherit-build-early/install.sh
@@ -4,4 +4,5 @@ set -e
 isLevelEnabled "debug" && set -x
 
 package_name=${ACI_NAME#aci-arch-*}
+[ x${package_name} == "xyaourt-auto" ] && exit 0
 su -c "yaourt -S  ${package_name} --noconfirm" yaourt
