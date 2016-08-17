ecc9f09160dceccce128b3b1979b70ecd795712a
bob <bob@blablacar.com>
2016-08-17 23:56:55 +0200

    aci-arch-ceph - start mds.

--- aci/archlinux/aci-arch-ceph/runlevels/builder/10.userAddCeph.sh
+++ aci/archlinux/aci-arch-ceph/runlevels/builder/10.userAddCeph.sh
@@ -3,5 +3,5 @@ set -e
 source /dgr/bin/functions.sh
 isLevelEnabled "debug" && set -x
 
-su -c "yaourt -S  adduser --noconfirm" yaourt
+su -c "yaourt -S  adduser lsb-release util-linux --noconfirm" yaourt
 chroot ${ROOTFS} useradd ceph
--- aci/archlinux/aci-arch-ceph/templates/dgr/runlevels/prestart-late/10.prepare-ceph.tmpl.sh
+++ aci/archlinux/aci-arch-ceph/templates/dgr/runlevels/prestart-late/10.prepare-ceph.tmpl.sh
@@ -4,17 +4,32 @@ source /dgr/bin/functions.sh
 isLevelEnabled "debug" && set -x
 
 {{if (eq .ceph.node.type "monitor" ) }}
-mkdir -p /var/lib/ceph/mon/{{.ceph.cluster.name}}-node1
-chown ceph: -R /var/lib/ceph/mon
-monmaptool --create --add node1 {{.pod.ip}} --fsid {{.ceph.fsid}} /tmp/monmap
-su -m ceph -c 'ceph-mon --cluster {{.ceph.cluster.name}} --mkfs -i node1 --monmap /tmp/monmap --keyring /tmp/{{.ceph.cluster.name}}.mon.keyring'
+
+mkdir -p /opt/ceph/mon/{{.ceph.cluster.name}}-mon{{.ceph.node.mon_number}}
+chown ceph: -R  /opt/ceph/mon
+monmaptool --create --add mon{{.ceph.node.mon_number}} {{.pod.ip}} --fsid {{.ceph.fsid}} /tmp/monmap
+su -m ceph -c 'ceph-mon --cluster {{.ceph.cluster.name}} --mkfs -i mon{{.ceph.node.mon_number}}  --monmap /tmp/monmap --keyring /tmp/{{.ceph.cluster.name}}.mon.keyring'
+
 {{else if  ( eq .ceph.node.type "osd" )}}
-#ceph osd create {{.ceph.fsid}}
-#mkdir -p /var/lib/ceph/osd/{{.ceph.cluster.name}}-{{.ceph.node.osd_number}}
-#ceph-osd -i {{.ceph.node.osd_number}} --mkfs --mkkey --cluster {{.ceph.cluster.name}}
-#ceph auth add osd.{{.ceph.node.osd_number}} osd 'allow *' mon 'allow rwx' -i /var/lib/ceph/osd/{{.ceph.cluster.name}}-{{.ceph.node.osd_number}}/keyring
-#chown ceph: -R /var/lib/ceph/osd
-#ceph osd crush add-bucket {{.ceph.node.name}} host
-#ceph osd crush move node1 root=default
+
+ceph --cluster {{.ceph.cluster.name}} osd create {{.ceph.fsid}} {{.ceph.node.osd_number}}
+mkdir -p  /opt/ceph/osd/{{.ceph.cluster.name}}-osd{{.ceph.node.osd_number}}
+ceph-osd -i {{.ceph.node.osd_number}} --mkfs --mkkey --cluster {{.ceph.cluster.name}}
+#ceph-authtool /var/lib/ceph/osd/{{.ceph.cluster.name}}.keyring --create-keyring --gen-key -n client.osd --cap mon 'allow profile osd'
+ceph --cluster {{.ceph.cluster.name}} auth del osd{{.ceph.node.osd_number}}
+ceph --cluster {{.ceph.cluster.name}} auth add osd{{.ceph.node.osd_number}} osd 'allow *' mon 'allow rwx' -i /opt/ceph/osd/{{.ceph.cluster.name}}-osd{{.ceph.node.osd_number}}/keyring
+chown ceph: -R /opt/ceph/osd
+ceph --cluster {{.ceph.cluster.name}} osd crush add-bucket {{.ceph.node.name}} host
+ceph --cluster {{.ceph.cluster.name}} osd crush move  {{.ceph.node.name}} root=default
+
 {{else if  ( eq .ceph.node.type "mds" )}}
+
+mkdir  -p /opt/ceph/mds/{{.ceph.cluster.name}}-mds{{.ceph.node.mon_number}}
+
+if [ ! -e  /opt/ceph/mds/mds{{.ceph.node.mds_number}}/keyring ]; then
+	ceph --cluster {{.ceph.cluster.name}}  /etc/ceph/{{.ceph.cluster.name}}.client.admin.keyring auth get-or-create mds{{.ceph.node.mds_number}} osd 'allow rwx' mds 'allow' mon 'allow profile mds' -o  /opt/ceph/mds/{{.ceph.cluster.name}}-mds.{{.ceph.node.mds_number}}/keyring
+	chown ceph.   /opt/ceph/mds/{{.ceph.cluster.name}}-mds{{.ceph.node.mds_number}}/keyring
+	chmod 600   /opt/ceph/mds/{{.ceph.cluster.name}}-mds{{.ceph.node.mds_number}}/keyring
+fi
+
 {{- end -}}
