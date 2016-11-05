358d8c3e8709a61184c9f3646db9b77d3c3c8de8
Arnaud Lemaire <alemaire@norad.fr>
2016-11-05 19:17:18 +0100

    add ggn example environment

--- bin/.travis-install.sh
+++ bin/.travis-install.sh
@@ -11,12 +11,12 @@ chmod +x dgr-update.sh
 
 
 version="v1.0.0"
-filename="rkt-${version}.tar.gz"
-url="https://github.com/coreos/rkt/releases/download/${version}/${filename}"
+filename="rkt.yml-${version}.tar.gz"
+url="https://github.com/coreos/rkt.yml/releases/download/${version}/${filename}"
 
 $(rkt version | grep "${version}") || {
-    mkdir -p "/tmp/rkt"
-    cd "/tmp/rkt"
+    mkdir -p "/tmp/rkt.yml"
+    cd "/tmp/rkt.yml"
 	wget $url
 	tar xvzf "${filename}" --strip=1
 	cp rkt stage1* /bin/
