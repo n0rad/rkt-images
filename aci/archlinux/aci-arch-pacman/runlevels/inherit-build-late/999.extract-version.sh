b42095248459704222b8ee86ed8638ddea3628b1
Arnaud Lemaire <alemaire@norad.fr>
2016-11-29 12:29:42 +0100

    fix version extraction

--- aci/archlinux/aci-arch-pacman/runlevels/inherit-build-late/999.extract-version.sh
+++ aci/archlinux/aci-arch-pacman/runlevels/inherit-build-late/999.extract-version.sh
@@ -3,7 +3,7 @@ set -e
 . /dgr/bin/functions.sh
 isLevelEnabled "debug" && set -x
 
-version=$(pacman -Qi ${ACI_NAME#aci-arch-*} 2> /dev/null | grep Version | cut -f2 -d: | tr -d '[[:space:]]')
+version=$(pacman -Qi ${ACI_NAME#aci-arch-*} 2> /dev/null | grep Version | cut -f2- -d: | tr -d '[[:space:]]')
 date=$(date -u '+%Y%m%d_%H%M%S')
 
 cat > /dgr/builder/attributes/version.yml <<EOF
