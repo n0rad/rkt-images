f37ef0f8259d9f21685e791adb6965fe1d2ddc94
Arnaud Lemaire <alemaire@norad.fr>
2016-12-05 13:40:54 +0100

    up

--- aci/arch/aci-arch-seafile-server/runlevels/build/install.sh
+++ aci/arch/aci-arch-seafile-server/runlevels/build/install.sh
@@ -5,15 +5,14 @@ isLevelEnabled "debug" && set -x
 
 # seafile package sux so :
 su -c "yaourt -S python2-pillow python2-setuptools mysql-python libselinux --noconfirm" yaourt
-useradd -u 500 seafile
+groupadd -g 500 seafile
+useradd -u 500 -g 500 seafile
 
 
 version=${ACI_VERSION%-*}
 url="https://bintray.com/artifact/download/seafile-org/seafile/seafile-server_${version}_x86-64.tar.gz"
 
-PROGRAM_PATH="${ROOTFS}/usr/bin"
-mkdir -p ${PROGRAM_PATH}
-wget $url -O /opt/seafile.tar.gz
-cd /opt
-tar xzf /opt/seafile.tar.gz
-
+wget $url -O /seafile.tar.gz
+mkdir /seafile-server && cd /seafile-server
+tar --strip=1 -xzf /seafile.tar.gz
+rm /seafile.tar.gz
