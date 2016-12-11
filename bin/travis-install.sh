066314b77309d1157d99eaab01d2eac1ebfbf235
Arnaud Lemaire <alemaire@norad.fr>
2016-12-11 14:41:35 +0100

    update travis

--- bin/travis-install.sh
+++ bin/travis-install.sh
@@ -10,13 +10,13 @@ chmod +x dgr-update.sh
 
 
 
-version="v1.0.0"
-filename="rkt.yml-${version}.tar.gz"
-url="https://github.com/coreos/rkt.yml/releases/download/${version}/${filename}"
+version="v1.21.0"
+filename="rkt-${version}.tar.gz"
+url="https://github.com/coreos/rkt/releases/download/${version}/${filename}"
 
 $(rkt version | grep "${version}") || {
-    mkdir -p "/tmp/rkt.yml"
-    cd "/tmp/rkt.yml"
+    mkdir -p "/tmp/rkt"
+    cd "/tmp/rkt"
 	wget $url
 	tar xvzf "${filename}" --strip=1
 	cp rkt stage1* /bin/
