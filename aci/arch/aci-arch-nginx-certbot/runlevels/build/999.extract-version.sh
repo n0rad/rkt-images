897ac48b34d7ee2b603fe539cbf2e7be3b26a45e
Arnaud Lemaire <alemaire@norad.fr>
2017-03-13 15:17:16 +0100

    extract certbot version

---
+++ aci/arch/aci-arch-nginx-certbot/runlevels/build/999.extract-version.sh
@@ -0,0 +1,15 @@
+#!/dgr/bin/busybox sh
+set -e
+. /dgr/bin/functions.sh
+isLevelEnabled "debug" && set -x
+
+NAME=certbot
+
+version=$(pacman -Qi ${NAME#aci-arch-*} 2> /dev/null | grep Version | rev | cut -f1 -d: | rev | tr -d '[[:space:]]')
+date=$(date -u '+%Y%m%d_%H%M%S')
+
+cat > /dgr/builder/attributes/version.yml <<EOF
+default:
+  version: "${version}"
+  date: "${date}"
+EOF
