d4fc4743a0b3225801b5b3a26d131ee76e77df50
Arnaud Lemaire <alemaire@norad.fr>
2017-07-17 13:00:15 +0200

    fix deluge start script

--- aci/arch/aci-arch-deluge/templates/run.tmpl.sh
+++ aci/arch/aci-arch-deluge/templates/run.tmpl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
+echo "Starting web ..."
+deluge-web -f --loglevel=info -c /config
 echo "Starting daemon ..."
 deluged -d --loglevel=info -c /config
-echo "Starting web ..."
-deluge-web --loglevel=info -c /config
