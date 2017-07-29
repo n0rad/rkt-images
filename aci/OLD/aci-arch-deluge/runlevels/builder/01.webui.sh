1c3109b769081ebd478741f4e8d56a3fcce2608a
Arnaud Lemaire <alemaire@norad.fr>
2017-07-29 23:25:45 +0200

    remove some old aci

--- aci/OLD/aci-arch-deluge/runlevels/builder/01.webui.sh
+++
@@ -1,7 +0,0 @@
-#!/dgr/bin/busybox sh
-set -e
-. /dgr/bin/functions.sh
-isLevelEnabled "debug" && set -x
-
-su -c "yaourt -S  python2-mako --noconfirm" yaourt
-su -c "yaourt -S  python2-service-identity --noconfirm" yaourt
