b30d556edcb4ea33058c87d51c668ee8883814b8
Arnaud Lemaire <alemaire@norad.fr>
2017-09-13 22:39:25 +0200

    fix plex cannot restart

--- aci/arch/aci-arch-plex-media-server/runlevels/prestart-early/10.copy-plugins.sh
+++ aci/arch/aci-arch-plex-media-server/runlevels/prestart-early/10.copy-plugins.sh
@@ -3,4 +3,8 @@ set -e
 . /dgr/bin/functions.sh
 isLevelEnabled "debug" && set -x
 
+rm -f '/var/lib/plex/Plex Media Server/plexmediaserver.pid'
+
+
+mkdir -p '/var/lib/plex/Plex Media Server'
 cp -R /plex-plugins/* '/var/lib/plex/Plex Media Server'
